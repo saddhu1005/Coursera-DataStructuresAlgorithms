37436401baf384f785e26f5fa056927fb703d021
saddhu1005 <saddhu1005@gmail.com>
2019-02-22 03:16:13 +0530

    done updates and fixes

--- Algorithm-on-Strings/Programming-Assignment-1/non_shared_substring/non_shared_substring.py
+++ Algorithm-on-Strings/Programming-Assignment-1/non_shared_substring/non_shared_substring.py
@@ -13,19 +13,16 @@ class SuffixTree:
 	def __init__ (self, text):
 		self.root = self.Node(None)
 		self.root.child[text[0]] = self.Node(text)
-		self.c = 0
 		for i in range(1, len(text)):
 			current = self.root
 			j = i
 			while j < len(text):
-				self.c += 1
 				if text[j] in current.child:
 					child = current.child[text[j]]
 					label = child.label
 					k = j + 1
 					while k - j <len(label) and text[k] == label[k - j]:
 						k += 1
-						self.c += 1
 					if k - j == len(label):
 						current = child
 						j = k
@@ -46,20 +43,17 @@ class Non_shared_Substr:
 	def __init__ (self, root, text):
 		self.start = 0
 		self.length = 1<<30
-		self.c = 0
 		for i in range(len(text)):
 			current = root
 			j = i
 			while j < len(text):
 				# print('BUG here'+ str(j))
-				self.c += 1
 				if text[j] in current.child:
 					child = current.child[text[j]]
 					label = child.label
 					k = j + 1
 					while k < len(text) and k - j < len(label) and text[k] == label[k - j]:
 						k += 1
-						self.c += 1
 						if k - j + 1 >= self.length:
 							k = len(text)
 							break
@@ -81,21 +75,11 @@ class Non_shared_Substr:
 					break
 
 
-
-
-def print_tree (root):
-	for child in root.child:
-		print(root.child[child].label)
-		print_tree(root.child[child])
-
-
 def solve (p, q):
 	result = p
 	q = q + '#'
 	tree = SuffixTree(q)
-	# print_tree(tree.root)
 	result = Non_shared_Substr(tree.root, p)
-	# print(tree.c,result.c)
 	return p[result.start : result.start + result.length]
 
 p = sys.stdin.readline ().strip ()
